667651f81454306dc435ba343b83e44deec22c08
RomanSevryuk <Sevryuk.Roman@outlook.com>
2022-03-04 01:18:45 +0300

    04.03.2022

---
+++ 03.js
@@ -0,0 +1,23 @@
+let i = console.log;
+
+let array = [3, 2, 1];
+i(array.join(" больше, чем "));
+
+/*let array1 = ["one","two","thre","four","five"];
+let array2 = ["1","2","3","4","5"];
+let array3 = ["one1","two2","thre3","four4","five5"];
+let array11 = array1[Math.floor(Math.random()*5)];
+let array22 = array1[Math.floor(Math.random()*5)];
+let array33 = array1[Math.floor(Math.random()*5)];
+//let array33 = array1[Math.floor(Math.random() * array3.length)];
+let you = ["You ", array11, array22, array33 + "!!!"].join("-");
+
+i(you);
+/*let scalyAnimals = ["ОДИН", "ДВА"];
+let a = ["1", "2"];
+let furryAnimalsAndscalyAnimals = a.concat(scalyAnimals, furryAnimals);
+cns(furryAnimalsAndscalyAnimals);
+cns(furryAnimals);
+cns(scalyAnimals);
+cns(a);
+*/
