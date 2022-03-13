138e64ec3bfd39ca6dfa4b610c15393acad3576c
RomanSevryuk <Sevryuk.Roman@outlook.com>
2022-03-14 01:42:06 +0300

    13.03.2022_04_exercise2.js

---
+++ 04_exercise2.js
@@ -0,0 +1,7 @@
+let myCrazyObject = {
+    "name": "Nelepii obect",
+    "some array": [7, 9, { purpose: "pytanitsa", number: 123 }, 3.3],
+    "random animal": "Bananovaia akyla"
+};
+
+console.log(myCrazyObject["some array"][2].number);
