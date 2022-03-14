b93451cbde7b10f3ee664f39d5cafb657407ce5e
RomanSevryuk <Sevryuk.Roman@outlook.com>
2022-03-14 22:38:05 +0300

    14.03.2022_html and cycle

---
+++ 05_while.js
@@ -0,0 +1,36 @@
+/*let sheep = 0;
+while (sheep <= 10) {
+    console.log("poschitano ovets " + sheep + "!");
+    sheep ++;
+}
+console.log("Ysnyl");*/
+
+/*let hello = 3;
+for (let i = 0; i < hello; i++) {
+    console.log("Privetyli!");
+}*/
+
+/*let animals = ["Lev", "Flamingo", "Medved", "Ydav"];
+for (let i = 0; i < animals.length; i++) {
+    console.log("V zooparke " + animals[i] + ".");
+}*/
+
+/*let imia = "Nik";
+for (let i = 0; i < imia.length; i++) {
+    console.log("v imeni bykvi " + imia [i]);
+}*/
+
+/*for (let x = 2; x < 10000; x = x * 2) {
+console.log(x);
+}*/
+
+/*for (let x = 3; x < 10000; x = x * 3) {
+    console.log(x);
+}
+*/
+
+let x = 3;
+while( x < 100) {
+    console.log(x);
+    x = x * 3;
+}
