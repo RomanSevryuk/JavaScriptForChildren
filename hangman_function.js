61136ca5368d2718d5780913d8b61117422c25c1
RomanSevryuk <Sevryuk.Roman@outlook.com>
2022-03-18 16:58:53 +0300

    18.03.2022_updateHangman_function

--- hangman_function.js
+++ hangman_function.js
@@ -13,8 +13,9 @@ let setupAnswerArray = function (word) {
 return answerArray;
 }
 /////////////
-let showPlayerProgress = function (answerArray) {
+let showPlayerProgress = function (answerArray, attempts) {
     alert("Слово состоит из " + answerArray.length + " букв  (" + answerArray.join(" ") + ").");
+    alert("Осталось " + attempts + " попытки(ок)!");
 }
 ////////////
 let getGuess = function () {
@@ -22,38 +23,38 @@ let getGuess = function () {
     return guess;
 }
 ///////////
-let updateGameState = function (guess, word, answerArray, remainingLetters) {
-    for (let j = 0; j < word.length; j++) 
+let updateGameState = function (guess, word, answerArray, remainingLetters, attempts) {
+    for (let j = 0; j < word.length; j++)
         if (answerArray[j] === "_") {
             if  (word[j] === guess.toLowerCase()) {
                 answerArray[j] = guess.toLowerCase();
-                remainingLetters --;
+                remainingLetters --;     
             }          
-            } else if (answerArray[j] === guess) {
+        } else if (answerArray[j] === guess) {
                 alert("Буква уже угадана!");
                 break;                
-            }
-            
-            return remainingLetters;
         }
-        
+        attempts[0] --;             
+        return remainingLetters;
+}                         
 //////////   
 let showAnswerAndCongratulatePlayer = function (answerArray) {
-alert("Отлично! Было загадано слово " + word + " Поздравляю!!!");
+alert('Отлично! Было загадано слово "' + word + '". Поздравляю!!!');
 }
 //-----------------------//
 let word = pickWord();
 let answerArray = setupAnswerArray(word);
 let remainingLetters = word.length;
-while (remainingLetters > 0) {
-   showPlayerProgress (answerArray);
+let attempts = [word.length * 2];
+while ((remainingLetters > 0) && (attempts > 0)) {      
+   showPlayerProgress (answerArray, attempts);
    let guess = getGuess();
    if (guess === null) {
     break;
 } else if (guess.length !== 1) {
     alert("Пожалуйста, введите только одну букву.");
 } else {
-    let correctGuesses = updateGameState(guess, word, answerArray, remainingLetters);       
+    let correctGuesses = updateGameState(guess, word, answerArray, remainingLetters, attempts);       
     remainingLetters = correctGuesses;       
 }
 }
